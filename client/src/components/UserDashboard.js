51b112e05a3c6e3cabf804ba77760bf0af16ae54
cecelia001 <diascece@gmail.com>
2023-03-13 16:50:42 -0400

    Css to home page

--- client/src/components/UserDashboard.js
+++ client/src/components/UserDashboard.js
@@ -18,12 +18,11 @@ useEffect(() => {
 
         <div className="dashboard">
 
-
-            <h2> My Dashboard </h2>
-            <div className = "row">
+            {/* <h2> My Dashboard </h2> */}
+            <div className = "box">
                 <div className="col-sm-4">
                 <div className="card">
-                    <img src="https://media.istockphoto.com/vectors/piggy-bank-vector-id983438172?k=20&m=983438172&s=170667a&w=0&h=jHNPxW-0NCZD5MM_PfTu78kG50qFCyuSxhA377pvCHs=" className="card-img-top" alt="piggybank"/>
+                    {/* <img src="https://media.istockphoto.com/vectors/piggy-bank-vector-id983438172?k=20&m=983438172&s=170667a&w=0&h=jHNPxW-0NCZD5MM_PfTu78kG50qFCyuSxhA377pvCHs=" className="card-img-top" alt="piggybank"/> */}
                     <div className="card-body">
                         <h5 className="card-title">Total Monthly Budget</h5>
                         <p className="card-text"> ${props.sumBudget} </p>
@@ -34,7 +33,7 @@ useEffect(() => {
                 
                 <div className="col-sm-4"> 
                 <div className="card">
-                    <img src="https://media.istockphoto.com/vectors/many-bank-cards-in-one-hand-vector-id1128868969?k=20&m=1128868969&s=612x612&w=0&h=Faxjl8SsDLzAgCFiIodnqigborUKgKLlYzPKCvkkTJ4=" className="card-img-top" alt="card"/>
+                    {/* <img src="https://media.istockphoto.com/vectors/many-bank-cards-in-one-hand-vector-id1128868969?k=20&m=1128868969&s=612x612&w=0&h=Faxjl8SsDLzAgCFiIodnqigborUKgKLlYzPKCvkkTJ4=" className="card-img-top" alt="card"/> */}
                     <div className="card-body">
                         <h5 className="card-title">Total Monthly Expenses</h5>
                         <p className="card-text"> ${props.sumMonthExpenses} </p>
@@ -45,7 +44,7 @@ useEffect(() => {
                 
                 <div className="col-sm-4"> 
                 <div className="card">
-                    <img src="https://img.freepik.com/free-vector/money-bag-background-design_1270-41.jpg?w=360" className="card-img-top" alt="money"/>
+                    {/* <img src="https://img.freepik.com/free-vector/money-bag-background-design_1270-41.jpg?w=360" className="card-img-top" alt="money"/> */}
                     <div className="card-body">
                         <h5 className="card-title">Difference</h5>
                         <p className="card-text" > $ {leftover(props.sumBudget, props.sumMonthExpenses)} </p>
